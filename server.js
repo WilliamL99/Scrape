b2c7d0006af8b187cf42428445f38a54a668964c
William Lan <wlan76@yahoo.com>
2019-01-27 20:30:54 -0800

    Update

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
-  axios.get("https://old.reddit.com/r/startrek/").then(function(response) {
+  axios.get("https://www.reddit.com/r/startrek/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
